59e48bdae4c65f18a5570675bb57948d6fc9e5f8
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2022-09-13 20:16:30 -0300

    feat: Create exists function to prevent unnecessary brew install

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,5 +1,13 @@
+#!/usr/bin/env zsh
+
 echo "\n ## HOMEBREW SETUP ##\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 brew bundle --verbose
+
---
+++ zshenv
@@ -0,0 +1,10 @@
+# FUNCTIONS
+
+function exists () {
+  # command -v is similar  to `which`
+  # $1 the first parameter used in exists function
+  # dev/null is a blackhole to throw away any error messages
+  # &1 means that the result will got to same place as the first part = dev/null
+  command -v $1 > /dev/null 2>&1
+}
+
