16ef36b5fd458d62fda6fed05944d6207dd56f33
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2022-09-20 20:48:13 -0300

    chore: Install global prettier

--- install_packages.zsh
+++ install_packages.zsh
@@ -6,6 +6,7 @@ brew bundle --verbose
 echo "\n## INSTALL NODE PACKAGES ##\n"
 
 npm install --global yarn
+npm install --global prettier
 
 echo "Global NPM Packages Installed:"
 npm list --global --depth=0
