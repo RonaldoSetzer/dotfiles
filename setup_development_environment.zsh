e85d962991927395dba27b413e3cfd5082604838
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2022-11-08 20:55:18 -0300

    feat: Add setup to terminfo with alacritty

--- setup_development_environment.zsh
+++ setup_development_environment.zsh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
 
+echo "\n## TERMINFO ##\n"
+
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+cd ..
+rm -rf ./alacritty
+
+
 echo "\n## SETUP FONTS ##\n"
 
 cp ./fonts/*/* ~/Library/Fonts/
